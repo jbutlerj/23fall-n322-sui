1de07bd22d87c613657ab3c27b99610265050601
jbutlerj <144062324+jbutlerj@users.noreply.github.com>
2023-11-16 16:00:24 -0500

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Card, Image, Grid, Button, GridColumn } from 'semantic-ui-react'  
+import React from "react";
+import { Card, Image, Grid, Button } from "semantic-ui-react";
 
 export default function Home() {
-  return (
-    <>
-      <Grid>
-        <Grid.Row columns='1'>
-          <Grid.Column>
-            <h1>Home</h1>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns='4'>
-          <Grid.Column width='8'>
-            <Card fluid>
-              <Image src='/penguin.jpeg' alt="Penguins" wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>Floral Haven</Card.Header>
-                <Card.Description>
-                  Matthew is a musician living in Nashville.
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-          <Grid.Column>
-            <p>This is placeholder text so we can see what content will look like.</p>
-            <Button color="blue" basic>Info</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </>
-  )
+    return (
+        <>
+            <Grid>
+                <Grid.Row columns="1">
+                    <Grid.Column>
+                        <h1>Home</h1>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns="4">
+                    <Grid.Column width="8">
+                        <Card fluid>
+                            <Image
+                                src="/penguin.jpeg"
+                                alt="Penguins"
+                                wrapped
+                                ui={false}
+                            />
+                            <Card.Content>
+                                <Card.Header>Floral Haven</Card.Header>
+                                <Card.Description>
+                                    Matthew is a musician living in Nashville.
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <p>
+                            This is placeholder text so we can see what content
+                            will look like.
+                        </p>
+                        <Button color="blue" basic>
+                            Info
+                        </Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </>
+    );
 }
--- src/pages/pokemon/[name].js
+++ src/pages/pokemon/[name].js
@@ -1,43 +1,54 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { Loader, Image } from 'semantic-ui-react';
+import React from "react";
+import { useRouter } from "next/router";
+import { Loader, Image } from "semantic-ui-react";
 
 export default function PokemonName() {
-    const [pokemonInfo, setPokemonInfo] = React.useState({ loading: true });
+    const [showInfo, setShowInfo] = React.useState({ loading: true });
     const router = useRouter();
-    
-    React.useEffect(function() {
-        if(pokemonInfo.name !== router.query.name && router.query.name) {
-            console.log('Load in Pokemon Info');
-            fetch(`https://pokeapi.co/api/v2/pokemon/${router.query.name}`)
+
+    React.useEffect(function () {
+        if (showInfo.show.name !== router.query.name && router.query.name) {
+            console.log("Load in TV Show Info");
+            fetch(`https://api.tvmaze.com/search/shows?q=/${router.query.name}`)
                 .then((r) => r.json())
                 .then(function (r) {
-                    setPokemonInfo(r);
+                    setShowInfo(r);
                 })
-                .catch((e) => setPokemonInfo({ loading: false, name: router.query.name }));
+                .catch((e) =>
+                    setShowInfo({ loading: false, name: router.query.name })
+                );
         }
     });
 
     return (
         <>
-            <h1>Pokemon Name: {router.query.name}</h1>
-            <Loader active={pokemonInfo.loading || pokemonInfo.name !== router.query.name } />
+            <h1>TV Show Name: {router.query.name}</h1>
+            <Loader
+                active={showInfo.loading || showInfo.name !== router.query.name}
+            />
             {
                 // if this statement
-                pokemonInfo.id ? (
+                showInfo.id ? (
                     // true - then do this
                     <>
-                        <Image src={pokemonInfo.sprites.front_default} alt="Pokemon"></Image>
-                        <h5>Height: {pokemonInfo.height}</h5>
-                        <h5>Weight: {pokemonInfo.weight}</h5>
+                        <Image
+                            src={showInfo.show.image.medium}
+                            alt={router.query.name}
+                        ></Image>
+                        <h5>Height: {showInfo.height}</h5>
+                        <h5>Weight: {showInfo.weight}</h5>
                     </>
-                    ) : (
-                        // false - else do this
+                ) : (
+                    // false - else do this
                     <>
-                        {(pokemonInfo.name !== router.query.name) ? <h2>Searching for Pokemon</h2> : <h2>Pokemon Not Found</h2>}
+                        {showInfo.name !== router.query.name ? (
+                            <h2>Searching for Pokemon</h2>
+                        ) : (
+                            <h2>Pokemon Not Found</h2>
+                        )}
                     </>
-                    )
+                )
             }
         </>
-    )
+    );
 }
